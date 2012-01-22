9d1b8fce6b2af56ea6358fb1178fb789e2ab8673
LuisEnriqueGarciaFernandez <luisenriqueg@gmail.com>
2012-01-22 19:03:32 +0100

    Added "we follow"

--- Hello/HelloWorld/src/Hello.java
+++ Hello/HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 		System.out.println("Goodbye World!");
+		System.out.println("We follow");
 	}
 
 }
